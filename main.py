f3511d10aed88c2de2fa9c457fe66f8ff99747ae
mallimuondu <mallimuondu@gmail.com>
2020-10-29 14:36:55 +0300

    made changes

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def gender():
     global female
     female = 'lady'
     if gend == 'male' or gend == 'm':
-        print('Thank you Lady',nam)
+        print('Thank you Mr',nam)
     elif gend == 'female' or gend == 'f':
-        print('Thank you Mr.',nam)
+        print('Thank you lady',nam)
     elif gend == 'diclose' or gend == 'do not want to diclose'or gend == 'd':
         print('Thank you ',nam,'! We respect your right to not tell us')
     elif gend == ' ' or gend == '':
@@ -52,13 +52,14 @@ def continues():
     if gb == 'E' or gb == 'e':
         pass
     else:
-        exit()
+        print('thankyou ',nam,'for singnig up the reverse game using team mode.BYE.come to play soon play back soon')
 continues()
     
 
 print('''
 ''')
-def game():
+def team():
+    global tem
     tem =input('''WHICH TEAM DO YOU WANT TO BE IN:
     team malli
     team nesh
@@ -73,63 +74,66 @@ def game():
             pass
         blue()
     print('welcome to team',tem)
-    maingame = input('''To proced in the game choose an option:
-    a.play
-    b.lern rules
-    ''')
-    if maingame == 'a' or maingame == 'play':
-        def main(re):
-            return re == re [::-1]
-        def main2():
-            re = input('pls input a word to see if it can be reversable: ')
-            if re == '' or re == ' ':
-                print('you have inputed nothing try agin')
+    def game():
+        maingame = input('''To proced in the game choose an option:
+        a.play
+        b.lern rules
+        ''')
+        if maingame == 'a' or maingame == 'play':
+            def play():
+                def main(re):
+                    return re == re [::-1]
+                def main2():
+                    re = input('pls input a word to see if it can be reversable: ')
+                    if re == '' or re == ' ':
+                        print('you have inputed nothing try agin')
+                        main2()
+                    ans = main(re)
+
+                    if ans:
+                        squares = []
+                        a = True
+                        while a == True:
+                            squares.append(1)
+                            print(squares)
+                            sum = 0
+                            for num in squares:
+                                sum += num
+                            print(re,'can be reversable.congratulation',nam,' you have gotten 1 point for team',tem,'now team',tem,'has',sum,'points')
+                            input1 = input("Do you want to try again:  ")
+                            if input1 == "yes":
+                                continue
+                            else:
+                                a = False
+                            break
+
+                    else:
+                        print(re,' cant be reversable.you do not have a point')
                 main2()
-            ans = main(re)
-
-            if ans:
-                print(re,'can be reversable.congratulation you have gotten 1 point')
-                squares = []
-                a = True
-                while a == True:
-                    squares.append(1)
-                    print(squares)
-                    sum = 0
-                    for num in squares:
-                        sum += num
-                    print(sum)
-                    break
-
+                gbt = input('input E to go back to the menu of the game or any anther key to quit the game: ')
+                if gbt == 'E' or gbt == 'e':
+                    play()
+                else:
+                    print('thankyou ',nam,'for playing the reverse game using team mode.BYE.come play back soon')
+                    exit()
+            play()
+        elif maingame =='b' or maingame == 'lern rools':
+            print(''' 
+            1. Enter word that you think can be read forwards and backwards
+            2. if it is correct you will score 1 point.
+            3. If it is wrong you get 0 points.''')
+            gb = input('input E to go back to the menu of the game or any anther key to quit the game: ')
+            if gb == 'E' or gb == 'e':
+                game()
             else:
-                print(re,' cant be reversable.you do not have a point')
-        main2()
-        gbt = input('input E to go back to the menu of the game or any anther key to quit the game: ')
-        if gbt == 'E' or gbt == 'e':
-            pass
-        else:
-            exit()
-        if tem == 'a':
-            print('now its blue team tern')
-            main()
-        elif tem == 'b':
-            print('now its red team tern')
-            main()
-    elif maingame =='b' or maingame == 'lern rools':
-        print(''' 
-        1. Enter word that you think can be read forwards and backwards
-        2. if it is correct you will score 1 point.
-        3. If it is wrong you get 0 points.''')
-        gb = input('input E to go back to the menu of the game or any anther key to quit the game: ')
-        if gb == 'E' or gb == 'e':
+                print('bye')
+                exit()
+        elif maingame == '' or maingame == ' ':
+            print('you have inputed nothing try again')
             game()
-        else:
-            print('bye')
-            exit()
-    elif maingame == '' or maingame == ' ':
-        print('you have inputed nothing try again')
-        game()
-        
-    elif maingame != 'a' or maingame != 'b':
-        print('you have inputed the wrong thing try again')
-        game()
-game()
+
+        elif maingame != 'a' or maingame != 'b':
+            print('you have inputed the wrong thing try again')
+            game()
+    game()
+team()
